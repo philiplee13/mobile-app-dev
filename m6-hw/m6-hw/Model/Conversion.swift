517228b23f320dd939a2d24d29c85130431f7bd9
philiplee13 <philipyjlee95@gmail.com>
2023-01-30 13:01:59 -0800

    m6 hw

---
+++ m6-hw/m6-hw/Model/Conversion.swift
@@ -0,0 +1,8 @@
+//
+//  Conversion.swift
+//  m6-hw
+//
+//  Created by Philip Lee on 1/30/23.
+//
+
+import Foundation
--- m6-hw/m6-hw/ViewController.swift
+++ m6-hw/m6-hw/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var amountToConvert: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func navigateToConvertedView(_ sender: Any) {
+        self.performSegue(withIdentifier: "toConvertedCurrency", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "toConvertedCurrency" {
+            let conversionView = segue.destination as! CurrencyConvertedViewController
+            conversionView.originalAmount = amountToConvert.text!
+        }
+    }
 }
 
